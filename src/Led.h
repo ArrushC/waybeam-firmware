0480349b27c2d05730ab536a123ee6d7d41018cb
Arrush <arrushchaturvedi@gmail.com>
2025-09-08 17:24:34 +0100

    feat(Waybeam): Add support for Waybeam

--- src/Led.h
+++ src/Led.h
@@ -3,5 +3,7 @@
 
 /**
  * ledForceOn and ledForceOff both override the normal ledBlinker behavior (which is controlled by main)
+ * ledTransmit provides visual feedback during radio transmissions
+ * ledReceive provides visual feedback for incoming radio messages
  */
-extern GpioVirtPin ledForceOn, ledBlink;
+extern GpioVirtPin ledForceOn, ledBlink, ledTransmit, ledReceive;
